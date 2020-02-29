d0c54f2d69c0adf3af43781bc763e02b279437a1
heaust <heaust.azure@gmail.com>
2020-02-29 15:22:24 +0530

    Cleaned up Boilerplate and Installed and Set up TailwindCSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-blue-700">
+     <h1 className="text-center pt-5 text-gray-400"> hello world<br/><br/></h1>
     </div>
   );
 }
