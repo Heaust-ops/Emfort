560983d93cd824c448ce37d3d38d05ad7742eef5
heaust <heaust.azure@gmail.com>
2020-05-19 12:31:11 +0530

    retry

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
