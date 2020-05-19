2af5850f47097084c7fa9f3cfd3cf7796bf99735
heaust <heaust.azure@gmail.com>
2020-05-19 11:33:48 +0530

    updated server

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
